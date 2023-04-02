cc740df3434ae47331dbc788d519051ad4f6ad66
Steven Fletcher <stevenfletcher17@gmail.com>
2023-04-01 19:19:18 -0600

    Comments

---
+++ frontend/src/pages/Movies.tsx
@@ -0,0 +1,4 @@
+export default function Movies() {
+  return <h1>Movie Collection</h1>;
+}
+// Ignore this
---
+++ frontend/src/pages/Podcast.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Podcast page
+
+export default function Podcast() {
+  return (
+    <div className="container">
+      <h1 className="display-4 mt-5 mb-4 text-center">My Podcast</h1>
+      <p className="lead text-center">
+        Check out <a href="https://baconsale.com/">baconsale.com</a> and follow
+        my podcast!
+      </p>
+    </div>
+  );
+}
